ca7abb61dd8bb34fc6242d89e7f64417cc933e1d
Katie Jundt <31411822+jundtkatie@users.noreply.github.com>
2018-01-30 19:55:13 -0600

    Add files via upload

---
+++ build/js/why_vegan_scripts.js
@@ -0,0 +1,71 @@
+function whatdoesitmean() {
+    var x = document.getElementById("whatdoesitmean");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function animals() {
+    var x = document.getElementById("animals");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function health() {
+    var x = document.getElementById("health");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function earth() {
+    var x = document.getElementById("earth");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function people() {
+    var x = document.getElementById("people");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function vegetarian() {
+    var x = document.getElementById("vegetarian");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function ethicalmeat() {
+    var x = document.getElementById("ethicalmeat");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function factoryfarms() {
+    var x = document.getElementById("factoryfarms");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
